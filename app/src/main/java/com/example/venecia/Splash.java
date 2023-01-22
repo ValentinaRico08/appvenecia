40354a40c08c807a0e43b5b33ebde461034eae4e
Valentina Taborda <valentina18tabordarico@gmail.com>
2023-01-22 12:24:54 -0500

    Se creo el splash

---
+++ app/src/main/java/com/example/venecia/Splash.java
@@ -0,0 +1,23 @@
+package com.example.venecia;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
